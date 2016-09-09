07424cee750d3347eedc6bf3de0a0c1572acef41
vicch <vicch7@gmail.com>
2016-09-08 21:15:55 -0300

    #002 in C

--- c/000-049/002.c
+++ c/000-049/002.c
@@ -1,95 +1,75 @@
-// https://leetcode.com/problems/add-two-numbers/
 #include <stdio.h>
 #include <stdlib.h>
 
-struct ListNode
-{
+struct ListNode {
     int val;
     struct ListNode* next;
 };
 
-struct ListNode* createList(int seq[], int len);
-struct ListNode* addLists(struct ListNode* l1, struct ListNode* l2);
+struct ListNode* makeList(int* nums, int count);
+void printList(struct ListNode* head);
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2);
 
 int main()
 {
     int seq1[10] = {1,1,0,4,7,2,7,3,0,1};
     int seq2[10] = {2,5,5,6,8,0,3,0,5,1};
-    struct ListNode *l1, *l2, *sum, *iter;
+    struct ListNode* l1 = makeList(seq1, 10);
+    struct ListNode* l2 = makeList(seq2, 10);
 
-    l1 = createList(seq1, 10);
-    l2 = createList(seq2, 10);
-    sum = addLists(l1, l2);
+    printList(l1);
+    printList(l2);
 
-    iter = sum;
-    while (iter != NULL) {
-        printf("%d\n", iter->val);
-        iter = iter->next;
-    }
+    struct ListNode* sum = addTwoNumbers(l1, l2);
+    printList(sum);
 
     return 0;
 }
 
-struct ListNode* createList(int seq[], int len)
+struct ListNode* makeList(int* nums, int count)
 {
-    struct ListNode *head, *cur, *next;
-    int i;
+    struct ListNode* head = malloc(sizeof(struct ListNode) * count);
+    struct ListNode* node = head;
 
-    head = cur = malloc(sizeof(struct ListNode));
-    cur->val = seq[0];
-
-    for (i = 1; i < len; i++) {
-        next = malloc(sizeof(struct ListNode));
-        next->val = seq[i];
-        cur->next = next;
-        cur = next;
+    for (; count > 0; count--, nums++, node++) {
+        node->val = *nums;
+        node->next = (count > 1) ? node + 1 : NULL;
     }
 
     return head;
 }
 
-struct ListNode* addLists(struct ListNode* l1, struct ListNode* l2)
+void printList(struct ListNode* head)
 {
-    struct ListNode *head, *cur, *next;
-    int val1, val2, sum, carry;
-
-    sum = l1->val + l2->val;
-    if (sum > 9) {
-        sum -= 10;
-        carry = 1;
-    } else {
-        carry = 0;
+    for (; head != NULL; head = head->next) {
+        printf("%d->", head->val);
     }
-    head = cur = malloc(sizeof(struct ListNode));
-    head->val = sum;
-    
-    while (l1->next != NULL || l2->next != NULL || carry > 0) {
-        if (l1->next != NULL) {
-            l1 = l1->next;
-            val1 = l1->val;
-        } else {
-            val1 = 0;
-        }
-            
-        if (l2->next != NULL) {
-            l2 = l2->next;
-            val2 = l2->val;
-        } else {
-            val2 = 0;
-        }
+    printf("\n");
+}
 
-        sum = val1 + val2 + carry;
-        if (sum > 9) {
-            sum -= 10;
-            carry = 1;
-        } else {
-            carry = 0;
-        }
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2)
+{
+    struct ListNode* head = NULL;
+    struct ListNode* tail = NULL;
+    int sum = 0, carry = 0;
 
-        next = malloc(sizeof(struct ListNode));
-        next->val = sum;
-        cur->next = next;
-        cur = next;
+    while (l1 != NULL || l2 != NULL || carry == 1) {
+        sum = carry + ((l1 == NULL) ? 0 : l1->val) + ((l2 == NULL) ? 0 : l2->val);
+        carry = sum / 10;
+        sum %= 10;
+
+        struct ListNode* new = malloc(sizeof(struct ListNode));
+        new->val = sum;
+        new->next = NULL;
+
+        if (tail != NULL) {
+            tail->next = new;
+            tail = new;
+        } else
+            head = tail = new;
+
+        l1 = (l1 == NULL) ? NULL : l1->next;
+        l2 = (l2 == NULL) ? NULL : l2->next;
     }
 
     return head;
