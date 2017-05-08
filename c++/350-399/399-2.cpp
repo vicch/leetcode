4ea28dc29baf3b35e17fcf49ab938222a2191c3a
Victor Chen <vicch7@gmail.com>
2017-05-08 14:49:36 -0300

    #399 in C++

---
+++ c++/350-399/399-2.cpp
@@ -0,0 +1,29 @@
+// Problem:
+// There is a list of sorted integers from 1 to n. Starting from left to right,
+// remove the first number and every other number afterward until reach the end
+// of the list. Repeat the previous step again, but this time from right to
+// left, remove the right most number and every other number from the remaining
+// numbers. Keep repeating the steps again, alternating left to right and right
+// to left, until a single number remains. Find the last number that remains
+// starting with a list of length n.
+// 
+// Example:
+// Input: 9
+// Output: 6
+// 1 2 3 4 5 6 7 8 9
+// 2 4 6 8
+// 2 6
+// 6
+// 
+// Solution:
+// The operation is recursive, for example, [1, 2, 3, 4, 5] becomes [2, 4] after
+// one run, which can be considered as [1, 2] multiplied by 2, and the next run
+// will be backwards. Use two functions recursively calling each other, and
+// return when the value becomes 1.
+
+class Solution {
+public:
+    int lastRemaining(int n) {
+        return n == 1 ? 1 : 2 * (1 + n / 2 - lastRemaining(n / 2));
+    }
+};
