11321b46af4ae539b5a4a294afa5ae52a3837003
vicch <vicch7@gmail.com>
2019-12-03 17:27:19 -0500

    #944 in C

--- 944/944.c
+++ 944/944.c
@@ -1,16 +1,16 @@
 int minDeletionSize(char ** array, int length) {
   if (length <= 1)
     return 0;
-  
+
   int del, i, j;
   char c;
-  
+
   del = 0;
   
   // Loop through columns, take first char of the column
   for (j = 0; (c = array[0][j]) != '\0'; j++) {
 
-  	// Loop through rows, first row can be skipped because it's the base value
+    // Loop through rows, first row can be skipped because it's the base value
     for (i = 1; i < length; i++) {
 
       // If decreaing sequence is found, increment result value and continue to next column
@@ -24,6 +24,6 @@ int minDeletionSize(char ** array, int length) {
       }
     }
   }
-  
+
   return del;
 }
