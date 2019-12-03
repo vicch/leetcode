d8813733bbd9a742dbb5386e3bc565ab259b8c2f
vicch <vicch7@gmail.com>
2019-12-03 18:14:12 -0500

    #944 in Python3

--- 944/944.c
+++ 944/944.c
@@ -3,24 +3,19 @@ int minDeletionSize(char ** array, int length) {
     return 0;
 
   int del, i, j;
-  char c;
 
   del = 0;
   
   // Loop through columns, take first char of the column
-  for (j = 0; (c = array[0][j]) != '\0'; j++) {
+  for (j = 0; array[0][j] != '\0'; j++) {
 
     // Loop through rows, first row can be skipped because it's the base value
     for (i = 1; i < length; i++) {
 
       // If decreaing sequence is found, increment result value and continue to next column
-      if (array[i][j] < c) {
+      if (array[i][j] < array[i-1][j]) {
         del += 1;
         break;
-
-      // If not decreasing, take this char as base value for next comparison
-      } else {
-        c = array[i][j];
       }
     }
   }
