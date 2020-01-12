3afd9c72e24cc7f6affb2fe519e8cf7ad02d5041
vicch <vicch7@gmail.com>
2020-01-11 19:41:13 -0500

    #9 in C, C++ and Go

--- 9/9.c
+++ 9/9.c
@@ -4,35 +4,28 @@ int isPalindrome(int x);
 
 int main()
 {
-    printf("%d:%d\n", 1, isPalindrome(1));
-    printf("%d:%d\n", 12321, isPalindrome(12321));
-    printf("%d:%d\n", 123321, isPalindrome(12321));
-    printf("%ld:%d\n", 2222222222, isPalindrome(2222222222));
-    printf("%d:%d\n", -2147447412, isPalindrome(-2147447412));
+	printf("%d:%d\n", 1, isPalindrome(1));
+	printf("%d:%d\n", 12321, isPalindrome(12321));
+	printf("%d:%d\n", 123321, isPalindrome(12321));
+	printf("%ld:%d\n", 2222222222, isPalindrome(2222222222));
+	printf("%d:%d\n", -2147447412, isPalindrome(-2147447412));
 
-    return 0;
+	return 0;
 }
 
 int isPalindrome(int x)
 {
-    long rev = 0;
-    int sign = 1;
-    int copy = x;
-
-    if (x < 0) {
-        rev = 0;
-
-    } else if (x < 10) {
-        rev = x;
-
-    } else {
-        while (x > 0) {
-            rev = rev * 10 + x % 10;
-            x /= 10;
-        }
-    }
-
-    rev *= sign;
-    
-    return (rev == copy);
+	if (x < 0 || (x % 10 == 0 && x != 0))
+		// All negative ints are not palindrome
+		return 0;
+	else if (x < 10)
+		// All single digit positive ints are palindrome
+		return 1;
+	
+	// Reverse digits of an int, it may overflow as int, so use a long
+	long rev = 0;
+	for (int cpy = x; cpy > 0; cpy /= 10)
+		rev = rev * 10 + cpy % 10;
+	
+	return x == rev;
 }
