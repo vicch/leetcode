5d9686055d528e7c98e43feec934301c495a5e5f
vicch <vicch7@gmail.com>
2021-07-30 19:15:58 -0400

    #70 in Python

---
+++ 0001-0099/0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,15 @@
+"""
+For each step n, the method to climb to it f(n) = f(n-2) + f(n-1). So it's basically calculating the nth number in
+Fibonacci sequence. For a bottom-up approach, keep 2 vars for f(n-2) and f(n-1) and calculate f(n), then swap and
+continue.
+"""
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 1, 1
+        for _ in range(n - 1):
+            a, b = b, a + b
+        return b
