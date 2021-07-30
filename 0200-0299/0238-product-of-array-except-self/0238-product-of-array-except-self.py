d3f4ef9a0a29192416644b9290e61f553e93fa59
vicch <vicch7@gmail.com>
2021-07-29 22:56:14 -0400

    Update comments

--- 0200-0299/0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0200-0299/0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,26 +1,26 @@
+"""
+Consider array [1, 2, 3, 4], the calculation of products array is basically:
+
+                     1  -+
+                  1  2   |
+               1  2  3  -+- The "left hand" numbers multiplied incrementally
+products = [_, _, _, _]
+            2  3  4     -+
+            3  4         |
+            4           -+- The "right hand" numbers multiplied incrementally
+
+By "incrementally" it means: 2 x 3 x 4 = (3 x 4) x 2, so if the result of 3 x 4 is already in a variable, it
+only needs to multiply by 2 to get the next value, and so on.
+
+So with 2 rounds (one for "left hand" numbers and one for "right hand" numbers) of incremental multiplication,
+the products array can be obtained.
+"""
 class Solution(object):
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-
-        # Consider array [1, 2, 3, 4], the calculation of products array is basically:
-        #
-        #                      1  -+
-        #                   1  2   |
-        #                1  2  3  -+- The "left hand" numbers multiplied incrementally
-        # products = [_, _, _, _]
-        #             2  3  4     -+
-        #             3  4         |
-        #             4           -+- The "right hand" numbers multiplied incrementally
-        #
-        # By "incrementally" it means: 2 x 3 x 4 = (3 x 4) x 2, so if the result of 3 x 4 is already in a variable, it
-        # only needs to multiply by 2 to get the next value, and so on.
-        #
-        # So with 2 rounds (one for "left hand" numbers and one for "right hand" numbers) of incremental multiplication,
-        # the products array can be obtained.
-
         products = [1] * len(nums)
 
         # Multiply "left hand" numbers
