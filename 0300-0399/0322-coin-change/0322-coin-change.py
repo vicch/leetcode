de14e99867148484861d9524e3400e7d8985b583
vicch <vicch7@gmail.com>
2021-08-05 22:43:19 -0400

    #322 update

--- 0300-0399/0322-coin-change/0322-coin-change.py
+++ 0300-0399/0322-coin-change/0322-coin-change.py
@@ -15,15 +15,15 @@ class Solution(object):
         :rtype: int
         """
         # Init to a value larger than the largest possible number of coins (using only coins of 1 to get the amount).
-        # Array is 0-indexed with 0 representing amount 0, so needs cache[n] for amount n.
-        cache = [amount + 1] * (amount + 1)
-        cache[0] = 0
+        # Array is 0-indexed with 0 representing amount 0, so needs n+1 elements for amount n.
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0
 
         for i in range(1, amount + 1):
             for coin in coins:
                 if i >= coin:
-                    # Improve solution of current amount if possible
-                    cache[i] = min(cache[i], cache[i - coin] + 1)
+                    # Improve solution if using current coin as last coin to reach this amount is better
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
 
         # If cache[n] is the initial value, it means there is no solution
-        return cache[amount] if cache[amount] < amount + 1 else -1
+        return dp[amount] if dp[amount] < amount + 1 else -1
