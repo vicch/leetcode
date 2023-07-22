d92d3533f772249c99e615239af57a16eb3f4cf1
vicch <vicch7@gmail.com>
2023-07-22 10:56:30 -0400

    #22 in Python

---
+++ 0001-0099/0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,33 @@
+"""
+It's really easy to handle this as a combinations problem, where generate(n) becomes:
+generate(1) * generate(n - 1) + generate(2) * generate(n - 2) + ...
+But the combinations will contain duplidates, e.g. () + ()() = ()() + ()
+
+It's actually easier as a linear string generation with backtracking:
+- For each step, either a "(" or ")" can be attached to current string.
+- "(" can be attached, unless there are already n of them.
+- ")" can be attached, unless there are more or equal number of ")" than "(".
+- It reaches the bottom when there are n "(" and ")".
+"""
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        def generate(s, l, r):
+            # Can be: len(s) == n * 2, because the values of l and r are checked and can never lead to invalid result.
+            if l == n and r == n:
+                result.append(s)
+                return
+            
+            if l < n:
+                generate(s + '(', l + 1, r)
+
+            if r < l:
+                generate(s + ')', l, r + 1)
+
+        result = []
+        generate('', 0, 0)
+
+        return result
