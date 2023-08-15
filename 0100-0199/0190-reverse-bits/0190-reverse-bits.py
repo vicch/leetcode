be87e51f8d3480566f0ff227dba0efca2add648f
vicch <vicch7@gmail.com>
2023-08-14 22:30:54 -0400

    #190 in Python

--- 0100-0199/0190-reverse-bits/0190-reverse-bits.py
+++ 0100-0199/0190-reverse-bits/0190-reverse-bits.py
@@ -1,6 +1,20 @@
 """
 To reverse the bits, take bits from lower end of input while right shifting, then push the bits to lower end of the
 reversed number while left shifting.
+
+Follow up: If this function is called many times, how would you optimize it?
+
+We can trade space for time, by pre-computing a lookup array for chunks of N bits and their respective reversed version.
+
+cache = [reverse(i) for i in range(256)]
+def reverse(n):
+    reversed = 0
+    for _ in range(8):
+        reversed = (reversed << 1) | (n & 1)
+        n >>= 1
+    return reversed
+
+Then instead of moving 1 bit per loop, move 8 bits in one go by looking up the result.
 """
 class Solution:
     # @param n, an integer
