412f5533afb5eebf1347ede326a70fb7f82bb106
vicch <vicch7@gmail.com>
2023-08-19 18:02:33 -0400

    #338 in Python

--- 0300-0399/0338-counting-bits/0338-counting-bits-2.py
+++ 0300-0399/0338-counting-bits/0338-counting-bits-2.py
@@ -8,6 +8,9 @@ Value   0 | 1 | 1 2 | 1 2 2 3 | 1 2 ...
 Section N's length is 2^N. And the values in sections N is just [bits[0] + 1, bits[1] + 1, ...]. So the result array
 can be obtained by using a pointer that repeatedly starts from index 0 and iterate 2^N times to read existing values
 and increment by 1 to populate the following values.
+
+Time: O(n)
+Space: O(n), including the output array
 """
 class Solution(object):
     def countBits(self, n):
