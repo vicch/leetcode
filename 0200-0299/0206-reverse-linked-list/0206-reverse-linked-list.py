7132df90878e079f2b33f6a7d6c89a3632572597
vicch <vicch7@gmail.com>
2023-08-19 18:18:33 -0400

    #206 in Python

--- 0200-0299/0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0200-0299/0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -1,3 +1,11 @@
+"""
+It's essentially building the reverse list by popping nodes from the original list and prepend it to the new list. Keep
+two pointers at the head of the original and reverse list, and init the reverse list as empty. Keep popping and
+prepending till the end of original list.
+
+Time: O(n)
+Space: O(1)
+"""
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, val=0, next=None):
