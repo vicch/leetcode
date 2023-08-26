f2c83323ad442d81fa65c668e7ab80ff260bdef9
vicch <vicch7@gmail.com>
2023-08-25 20:29:05 -0400

    #704 in Python

---
+++ 0700-0799/0704-binary-search/0704-binary-search.py
@@ -0,0 +1,23 @@
+"""
+Pay attention to boundary conditions.
+
+Time: O(logn)
+Space: O(1)
+"""
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return -1
