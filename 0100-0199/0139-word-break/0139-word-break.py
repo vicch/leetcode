07777287d9c72c740ec351f32c65f834802eef58
Victor Chen <vicch7@gmail.com>
2024-11-18 22:09:32 -0500

    #139

--- 0100-0199/0139-word-break/0139-word-break.py
+++ 0100-0199/0139-word-break/0139-word-break.py
@@ -1,13 +1,20 @@
 """
-Consider DP:
+[NEED_REVIEW]
 
-- If sub-string s[0:n] can be spelled using dict words, and the following m chars matches one of the words in the dict,
-then sub-string s[0:n+m] can be spelled using dict words.
-- In contrast, if sub-string s[0:n] cannot be spelled, then even if the following chars match any of the words, there is
-no way the whole string can be spelled using dict words, and this branch can be cut.
+A typical bottom-up DP problem: if s[:x] can be spelled using dict words, and s[x:y] is a word as well, the s[:y] can be spelled. In other
+words, for each index i, we can check if there exists an index 0 < j < i, which is reachable by itself, and s[j:i] is a word.
 
-Iterate all locations of the string while maintaining the DP array. The last value of DP array indicates whether the
-whole string can be spelled.
+The reachable status for all indice can be initialized as False in a DP array, and there should be an additional True off the start of the array
+to represent the starting point.
+
+N = len(s)
+M = len(wordDict)
+
+Time complexity:
+- O(N^2), for the nested loops.
+
+Space complexity:
+- O(N + M), for word set and DP array.
 """
 class Solution(object):
     def wordBreak(self, s, wordDict):
@@ -16,25 +23,18 @@ class Solution(object):
         :type wordDict: List[str]
         :rtype: bool
         """
-        sl = len(s)
-        dp = [True] + [False] * sl
+        words = set(wordDict)
+        n = len(s)
 
-        for i in range(sl + 1):
-            # If there is no way to reach this char using dict words, it cannot be a starting point
-            if not dp[i]:
-                continue
+        # Init DP, with first slot as reachable
+        dp = [True] + [False] * n
 
-            for word in wordDict:
-                l = len(word)
+        for i in range(1, n + 1):
+            for j in range(i):
+                # If j was reachable and [j:i] is a word, then o is reachable.
+                if dp[j] and s[j:i] in words:
+                    dp[i] = True
+                    # No need to check further for i.
+                    break
 
-                # 2 situations when this word is not needed to check:
-                # - If word is longer than the remaining part of string.
-                # - If there is already a way to reach the same location as appending this word.
-                if i+l > sl or dp[i+l]:
-                    continue
-
-                # If the next N letters match the word, the spell-able sub-string can be extended by this word.
-                if s[i:i+l] == word:
-                    dp[i+l] = True
-
-        return dp[-1]
+        return dp[n]
